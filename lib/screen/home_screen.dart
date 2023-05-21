d482a907bd27b473df7345401895d53f228269b4
D0MHan <xlzlxkzk11@gmail.com>
2023-05-21 16:26:08 +0900

    first commit

---
+++ lib/screen/home_screen.dart
@@ -0,0 +1,122 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  DateTime firstDay = DateTime.now(); // 현재 시간
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.pink[100],
+      body: SafeArea(
+        top: true,
+        bottom: false,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+
+          children: [
+            _DDay(
+              onHeartPressed: onHeartPressed,
+              firstDay: firstDay,
+            ),
+            _CoupleImage(),
+          ],
+        ),
+      ),
+    );
+  }
+  void onHeartPressed() {
+    showCupertinoDialog(
+      context: context, 
+      builder: (BuildContext context) {
+        return Align(
+          alignment: Alignment.bottomCenter,
+          child: Container(
+            color: Colors.white,
+            height: 300,
+            child: CupertinoDatePicker(
+              mode: CupertinoDatePickerMode.date,
+              onDateTimeChanged: (DateTime date) {
+                setState(() {
+                  firstDay = date;
+                });
+              },
+            )
+          ),
+        );
+      },
+      barrierDismissible: true,
+    );
+  }
+}
+
+class _DDay extends StatelessWidget {
+  final GestureTapCallback onHeartPressed;
+  final DateTime firstDay;
+  _DDay({
+    required this.onHeartPressed,
+    required this.firstDay,  
+  });
+  @override
+  Widget build(BuildContext context) {
+    final textTheme = Theme.of(context).textTheme;
+    final now = DateTime.now();
+
+    return Column(
+      children: [
+        const SizedBox(height: 16.0),
+        Text(
+          'U&I',
+          style: textTheme.headline1,
+        ),
+        const SizedBox(height: 16.0),
+        Text(
+          '우리 처음 만난 날',
+          style: textTheme.bodyText1,
+        ),
+        Text(
+          //'2021.11.23',
+          '${firstDay.year}.${firstDay.month}.${firstDay.day}',
+          style: textTheme.headline2,
+        ),
+        const SizedBox(height: 16.0),
+        IconButton(
+          iconSize: 60.0,
+          onPressed: onHeartPressed,
+          icon: Icon(
+            Icons.favorite,
+            color: Colors.red,
+          ),        
+        ),
+        const SizedBox(height: 16.0),
+        Text(
+          //'D+365',
+          'D+${DateTime(now.year, now.month, now.day).difference(firstDay).inDays + 1}',
+          style: textTheme.headline2,
+        ),
+      ],
+    );
+  }
+}
+
+class _CoupleImage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Center(
+        child: Image.asset(
+          'asset/img/middle_image.png',
+          //이미지 높이를 화면 높이의 반으로 설정
+          height: MediaQuery.of(context).size.height / 2,
+        ),
+      ),
+    );
+  }
+}
